1dd0b74df3d6d052a96c9bbce25289175c22c6e8
yulikexuan <yulikexuan@gmail.com>
2018-06-20 16:35:24 -0400

    Initial Commit

---
+++ src/test/java/com/yulikexuan/clilab/ClilabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yulikexuan.clilab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClilabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
